ddb977d4360d5059de1125dc1bc1d98411ca116e
Tyler Schwemley <tjschwem@gmail.com>
2014-11-03 09:40:13 -0500

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// Include navigation template
+include '../templates/navigation.html';
+
+// Include home template
+include '../templates/home.html';
+
+// Include about template
+include '../templates/about.html';
+
+// Include resume template
+include '../templates/resume.html';
+
+// Include footer template
+include '../templates/footer.html';
