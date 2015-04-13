8813857db8e341f207ba13d1a885f29126155ccd
Tyler Schwemley <tjschwem@gmail.com>
2015-04-13 16:04:25 -0400

    Simple design initial commit

--- public/index.php
+++ public/index.php
@@ -1,16 +1,2 @@
 <?php
-
-// Include navigation template
-include '../templates/navigation.html';
-
-// Include home template
-include '../templates/home.html';
-
-// Include about template
-include '../templates/about.html';
-
-// Include resume template
-include '../templates/resume.html';
-
-// Include footer template
-include '../templates/footer.html';
+echo 'hi';
