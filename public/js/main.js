3cab97bc1d9b32d39e94f1341fb492f398758f4d
Tyler Schwemley <tjschwem@gmail.com>
2015-05-25 01:56:46 -0400

    Altered function ordering

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,36 @@
 $(document).ready(function() {
     var textWrapper = $('#text-wrapper'), 
         pause = $('#pause'),
+        skip = $('#skip'),
+        restart = $('#restart'),
         isPaused = false;
-        
+
     jQuery.get('/content.txt', function(data) {
         textWrapper.typed({
             strings: ["Hello :)^500 My name is Tyler^500; thanks for visiting!^500", data],
             typeSpeed: 0,
             contentType: 'html'
         });
-
-        pause.on('click', function() {
-            if (!isPaused) {
-                pause.text('Play');
-                textWrapper.data('typed').pauseTyping();
-                isPaused = true;
-            } else {
-                pause.text('Pause');
-                textWrapper.data('typed').continueTyping();
-                isPaused = false;
-            }
-        });
     });
 
+    pause.on('click', function() {
+        if (!isPaused) {
+            pause.text('Play');
+            textWrapper.data('typed').pauseTyping();
+            isPaused = true;
+        } else {
+            pause.text('Pause');
+            textWrapper.data('typed').continueTyping();
+            isPaused = false;
+        }
+    });
+
+    skip.on('click', function() {
+        textWrapper.data('typed').pauseTyping();
+
+        jQuery.get('/content.txt', function(data) {
+            textWrapper.html(''); 
+            textWrapper.html(data);
+        });
+    });
 });
