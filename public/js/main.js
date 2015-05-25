697d02ed8e9708eb8708be95b41aa3b99f54261d
Tyler Schwemley <tjschwem@gmail.com>
2015-05-25 00:43:50 -0400

    Updated pause event handler

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,26 @@
 $(document).ready(function() {
-    // var textWrapper = $('#text-wrapper'), 
-    //     pause = $('#pause'),
-    //     isPaused = false;
+    var textWrapper = $('#text-wrapper'), 
+        pause = $('#pause'),
+        isPaused = false;
         
-
     jQuery.get('/content.txt', function(data) {
-        $('#text-wrapper').typed({
+        textWrapper.typed({
             strings: ["Hello :)^500 My name is Tyler^500; thanks for visiting!^500", data],
             typeSpeed: 0,
             contentType: 'html'
         });
 
-        $('#pause').on('click', function() {
-                $('#pause').text('Play');
-                $('#text-wrapper').data('typed').pauseTyping();
+        pause.on('click', function() {
+            if (!isPaused) {
+                pause.text('Play');
+                textWrapper.data('typed').pauseTyping();
+                isPaused = true;
+            } else {
+                pause.text('Pause');
+                textWrapper.data('typed').continueTyping();
+                isPaused = false;
+            }
         });
     });
+
 });
